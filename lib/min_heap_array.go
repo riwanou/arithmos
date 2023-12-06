c0cfd4f05857be2ffbef026cbdac731658898ca9
Eros Chan ()28720888) <eros.chan@etu.sorbonne-universite.fr>
2023-12-06 14:46:33 +0100

    feat: internal functions array manipulation

--- lib/min_heap_array.go
+++ lib/min_heap_array.go
@@ -2,6 +2,19 @@ package lib
 
 type MinHeapArray struct {
 	array []KeyInt
+	size  int
+}
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+func left(i int) int {
+	return (2 * i) + 1
+}
+
+func right(i int) int {
+	return (2 * i) + 2
 }
 
 func NewMinHeapArray() *MinHeapArray {
