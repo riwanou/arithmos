9b3bfbc2c185aa7998fcf731811e2ff1e4625515
Eros Chan ()28720888) <eros.chan@etu.sorbonne-universite.fr>
2023-12-06 14:46:33 +0100

    feat: internal functions array manipulation

--- lib/min_heap_array.go
+++ lib/min_heap_array.go
@@ -1,7 +1,43 @@
 package lib
 
 type MinHeapArray struct {
-	array []KeyInt
+	array       []KeyInt
+	currentSize int
+}
+
+func (heap *MinHeapArray) root() KeyInt {
+	// Check if array is not empty
+	if heap.currentSize == 0 {
+		panic("Error: Unable to get root key because heap is empty!")
+	}
+	return heap.array[0]
+}
+
+func (heap *MinHeapArray) parent(i int) KeyInt {
+	// Check if index i doesn't exists
+	if i > heap.currentSize {
+		panic("Error: Unable to get parent key because index match any existing key!")
+	}
+	computeIndex := (i - 1) / 2
+	return heap.array[computeIndex]
+}
+
+func (heap *MinHeapArray) left(i int) KeyInt {
+	// Check if index i doesn't exists
+	if i > heap.currentSize {
+		panic("Error: Unable to get left key because index match any existing key!")
+	}
+	computeIndex := (2 * i) + 1
+	return heap.array[computeIndex]
+}
+
+func (heap *MinHeapArray) right(i int) KeyInt {
+	// Check if index i doesn't exists
+	if i > heap.currentSize {
+		panic("Error: Unable to get right key because index match any existing key!")
+	}
+	computeIndex := (2 * i) + 2
+	return heap.array[computeIndex]
 }
 
 func NewMinHeapArray() *MinHeapArray {
